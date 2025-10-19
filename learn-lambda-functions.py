dc2110c55f7596f9d4f809af86fb112d86c4bf2a
Melanie Almeida <almeida-melanie@proton.me>
2025-10-19 18:02:14 +0200

    step 32

--- learn-lambda-functions.py
+++ learn-lambda-functions.py
@@ -4,6 +4,9 @@ def add_expense(expenses, amount, category):
 def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
-
+    
 def total_expenses(expenses):
-    pass
+    return sum(map(lambda expense: expense['amount'], expenses))
+    
+def filter_expenses_by_category(expenses, category):
+    return filter(lambda expense: expense['category'] == category, expenses)
