4edf8297e4ab7c2cb0a6411d4680751328def3df
Melanie Almeida <almeida-melanie@proton.me>
2026-01-05 15:22:38 +0100

    step 35

--- Part 2/algorithm-design-shortest-path.py
+++ Part 2/algorithm-design-shortest-path.py
@@ -6,6 +6,9 @@ my_graph = {
 }
 
 def shortest_path(graph, start):
-    unvisited = []
-    for i in graph:
-        unvisited.append(i)
+    unvisited = list(graph)
+    distances = {}
+    paths = {node: [] for node in graph}
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+    
+shortest_path(my_graph, 'A')
